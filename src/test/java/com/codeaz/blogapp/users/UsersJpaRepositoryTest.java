dc800f0dad57db1ed1321f25b1b5f36cde172efb
getabalew7 <getabalew7@gmail.com>
2023-07-12 19:12:37 +0200

    creating a unit test for the UserRepository

--- src/test/java/com/codeaz/blogapp/users/UsersJpaRepositoryTest.java
+++ src/test/java/com/codeaz/blogapp/users/UsersJpaRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @DataJpaTest
 @ActiveProfiles("test")
 public class UsersJpaRepositoryTest {
@@ -15,10 +17,14 @@ public class UsersJpaRepositoryTest {
     @Test
     @Order(1)
     void can_create_users() throws Exception {
-        var user = UserEntity.builder().userName("getch")
+        var userEntity = UserEntity.builder().userName("getch")
                 .email("getch@gmail.com")
                 .build();
-        userRepository.save(user);
+        var user = userRepository.save(userEntity);
+        assertNotNull(user);
+        assertNotNull(user.getId());
+        assertEquals(userEntity.getId(), user.getId());
+        assertEquals(userEntity.getUserName(), user.getUserName());
     }
     @Test
     @Order(2)
@@ -28,6 +34,56 @@ public class UsersJpaRepositoryTest {
                 .build();
         userRepository.save(user);
         var oldUser = userRepository.findAll();
-        Assertions.assertEquals(oldUser.size(),1);
+        assertNotNull(oldUser);
+        assertEquals(oldUser.size(),1);
+    }
+    @Test
+    public void can_find_user_by_id() throws Exception {
+        var user = UserEntity.builder().userName("getch")
+               .email("getch@gmail.com")
+               .build();
+        userRepository.save(user);
+        var oldUser = userRepository.findById(user.getId());
+        assertNotNull(oldUser);
+        assertEquals(oldUser.get().getId(), user.getId());
+    }
+    @Test
+    public void can_find_user_by_name() throws Exception {
+        var user = UserEntity.builder().userName("getch")
+              .email("getch@gmail.com")
+              .build();
+        userRepository.save(user);
+        var oldUser = userRepository.findByUserName(user.getUserName());
+        assertNotNull(oldUser);
+        assertEquals(oldUser.get(0).getId(), user.getId());
+    }
+    @Test
+    public void can_update_user() throws Exception {
+        var user = UserEntity.builder().userName("getch")
+              .email("getch@gmail.com")
+              .build();
+        userRepository.save(user);
+        var oldUser = userRepository.findByUserName(user.getUserName());
+        assertNotNull(oldUser);
+        assertEquals(oldUser.get(0).getId(), user.getId());
+        user.setEmail("updatedgech@gmail.com");
+        userRepository.save(user);
+        var newUser = userRepository.findByUserName(user.getUserName());
+        assertNotNull(newUser);
+        assertEquals(newUser.get(0).getId(), user.getId());
+        assertEquals(newUser.get(0).getEmail(), user.getEmail());
+    }
+    @Test
+    public void can_delete_user() throws Exception {
+        var user = UserEntity.builder().userName("getch")
+             .email("getch@gmail.com")
+             .build();
+        userRepository.save(user);
+        var oldUser = userRepository.findById(user.getId());
+        assertNotNull(oldUser);
+        assertEquals(oldUser.get().getId(), user.getId());
+        userRepository.delete(user);
+        var newUser = userRepository.findById(user.getId());
+        assertEquals(newUser.isEmpty(), true);
     }
 }
